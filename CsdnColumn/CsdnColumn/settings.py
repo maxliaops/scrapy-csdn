cb67c9b48f264f8f3656064a70b34f515fd47c4d
Max Liao <maxliaops@gmail.com>
2014-04-30 05:49:03 -0400

    init commit
    Signed-off-by: Max Liao <maxliaops@gmail.com>

---
+++ CsdnColumn/CsdnColumn/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for CsdnColumn project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'CsdnColumn'
+
+SPIDER_MODULES = ['CsdnColumn.spiders']
+NEWSPIDER_MODULE = 'CsdnColumn.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'CsdnColumn (+http://www.yourdomain.com)'
